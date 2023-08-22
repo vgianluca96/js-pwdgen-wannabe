2de903bc9e9a112eb0b13702e75ab95e3c7bfa75
vgianluca96 <gianluca.vallese@hotmail.com>
2023-08-22 16:55:14 +0200

    password gen versione 2
    bonus con inserimento numero random sommato a numero input

--- main.js
+++ main.js
@@ -3,9 +3,18 @@
 let Name = prompt ('Digita il tuo nome');
 let Surname = prompt ('Digita il tuo cognome');
 let Color = prompt('Digita il tuo colore preferito');
+let InputNumber = prompt ('Digita un numero');
+
+// Creazione variabile con numero random
+let RandomNumber = Math.random() *100;
+RandomNumber = Math.floor(RandomNumber);
+console.log(RandomNumber);
+
+// Creazione numero per password
+let FinalNumber = RandomNumber + Number(InputNumber);
 
 // Creazione variabile password
-let Password = Name + Surname + Color + '23';
+let Password = Name + Surname + Color + String(FinalNumber);
 
 // Stampa password nella console
 console.log(Password);
