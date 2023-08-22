332aee1a3f691ab69471cc5256a8c7f1ffa4f6bc
vgianluca96 <gianluca.vallese@hotmail.com>
2023-08-22 14:48:11 +0200

    aggiunta commenti

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 
+// Prompt di richiesta dati
 let Name = prompt ('Digita il tuo nome');
 let Surname = prompt ('Digita il tuo cognome');
 let Color = prompt('Digita il tuo colore preferito');
 
+// Creazione variabile password
 let Password = Name + Surname + Color + '23';
 
-
+// Stampa password nella console
 console.log(Password);
 
+// Stampa password nella pagina
 document.getElementById('psw').innerHTML = Password;
